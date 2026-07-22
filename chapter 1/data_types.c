173e4948b7dd041038def800718a57fa1e22dedd
Alok Paswan <alokpaswan.dev@gmail.com>
2026-07-23 00:53:59 +0530

    adding data types

---
+++ chapter 1/data_types.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int a = 19;
+
+    float b;
+    b = 1.33;
+
+    char c;
+    c = 'a';
+
+    printf("The value of a is %d\n" , a);
+    printf("The value of b is %f\n" , b);
+    printf("The value of c is %c\n" , c);
+
+
+    return 0;
+}
