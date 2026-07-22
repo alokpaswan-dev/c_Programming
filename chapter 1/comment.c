6f379b5662bf2c99cad202eff53194f3bf716d11
Alok Paswan <alokpaswan.dev@gmail.com>
2026-07-23 01:01:58 +0530

    adding comment

---
+++ chapter 1/comment.c
@@ -0,0 +1,15 @@
+/*
+hi
+i am alok
+i am leaning c language
+*/
+#include <stdio.h>
+
+int main(){
+
+    // I am writing this just for fun.
+    printf("Hello world");
+    // Hello world how are you.
+
+    return 0;
+}
