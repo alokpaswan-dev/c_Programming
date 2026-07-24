546af794121c6d1f88fa048796acf924dc22144e
Alok Paswan <alokpaswan.dev@gmail.com>
2026-07-24 23:17:43 +0530

    add volume of cyclinder

---
+++ chapter1_practice/ques3.c
@@ -0,0 +1,11 @@
+// vloume of cylcinder 
+#include <stdio.h>
+int main(){
+    float  rad , height;
+    rad = 40;
+    height = 33;
+
+    printf("Volume of cyclinder =  %.2f\n" , 3.14 * rad * rad * height);
+
+    return 0;
+}
